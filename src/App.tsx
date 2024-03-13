e853dbe5b1563773010df8e798388491d2da22e2
Elijah Melton <elimelt@uw.edu>
2024-03-13 12:51:05 -0700

    added welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Availability from './components/Availability/Availability';
 import Personal from './components/Personal/Personal';
+import Welcome from './components/Welcome/Welcome';
+import { getNextMonday } from './utils/time';
 import './App.css';
 
+const renderWarning = (lastSignup: any) => {
+  if (!lastSignup) return null;
+
+  const nextMonday = getNextMonday(new Date());
+  const lastSignupDateForWeekOf = new Date(lastSignup.dateForWeekOf);
+
+  if (nextMonday.getDate() !== lastSignupDateForWeekOf.getDate())
+    return null;
+
+  return (
+    <div className="warning">
+      <h2>Warning</h2>
+      <p>
+        You have already signed up for a mock interview on {lastSignup.date} at {lastSignup.time}. If you sign up again, your previous sign up will be overwritten.
+      </p>
+    </div>
+  );
+}
+
 function App() {
-  const [step, setStep] = useState(1); // Step state to track which part of the form the user is on
+  const [step, setStep] = useState(0); // Step state to track which part of the form the user is on
+  const [lastSignup, setLastSignup] = useState(null); // State to store the last sign up data
+
+  useEffect(() => {
+    const lastSignupString = localStorage.getItem('lastSignup');
+    if (lastSignupString) setLastSignup(JSON.parse(lastSignupString));
+    else setLastSignup(null);
+  }
+  , []);
 
   // Function to move to the next step
   const nextStep = () => {
@@ -19,16 +48,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to Sign Up</h1>
+        <h1>Mock Interview Sign Up</h1>
       </header>
       <div className="form-container">
+        {step === 0 && <Welcome />} {/* Display Welcome component if step is 0 */}
         {step === 1 && <Personal />} {/* Display Personal component if step is 1 */}
         {step === 2 && <Availability />} {/* Display Availability component if step is 2 */}
       </div>
       <div className="buttons">
         {step > 1 && <button onClick={prevStep}>Previous</button>} {/* Display previous button if step is greater than 1 */}
-        {step < 2 && <button onClick={nextStep}>Next</button>} {/* Display next button if step is less than 2 */}
+        {step > 0 && step < 2 && <button onClick={nextStep}>Next</button>} {/* Display next button if step is less than 2 */}
+        {step === 0 && <button onClick={nextStep}>Sign Up</button>} {/* Display start button if step is 0 */}
       </div>
+      {renderWarning(lastSignup)} {/* Display warning message */}
     </div>
   );
 }
---
+++ src/components/Welcome/Welcome.tsx
@@ -0,0 +1,15 @@
+
+
+function Welcome() {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <p>
+        If you're looking to sign up for a mock interview, you're in the right place!
+      </p>
+
+    </div>
+  );
+}
+
+export default Welcome;
