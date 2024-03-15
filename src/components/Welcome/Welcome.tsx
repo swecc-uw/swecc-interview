26a6c736dedf1b84ec7b0ab68f83e3758b9ede51
Elijah Melton <elimelt@uw.edu>
2024-03-15 12:42:59 -0700

    test deploy

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -5,7 +5,7 @@ function Welcome() {
     <div>
       <h1>Welcome!</h1>
       <p>
-        If you're looking to sign up for a mock interview, you're in the right place!
+        If you're looking to sign up for a mock interview, you're in the right place! Login coming soon...
       </p>
       {/* <Auth /> */}
 
