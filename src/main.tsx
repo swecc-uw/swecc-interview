59f43d4a863421a7e7b525ad1f2272483b6bc122
Elijah Melton <elimelt@uw.edu>
2024-04-03 19:58:51 -0700

    emergency switch to hash router for ghpages compat

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename='/pairing-client'>
+  <HashRouter basename='/'>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>,
+  </HashRouter>,
 )
