7e82c7fe1c9a918104ea7164e0926c08de349fcd
Advay Patil <advaypatil27@gmail.com>
2024-12-27 14:47:06 -0800

    Remove unnecessary components

--- src/pages/admin/ReportDashboard.tsx
+++ src/pages/admin/ReportDashboard.tsx
@@ -3,40 +3,15 @@ import {
   Input,
   Select,
   Spinner,
-  Text,
   VStack,
   Box,
   Heading,
-  Button,
   useToast,
-  HStack,
-  Code,
 } from '@chakra-ui/react';
-import { ChevronDown, ChevronUp } from 'lucide-react';
 import { getAllReport } from '../../services/report';
 import { Report, ReportType } from '../../types';
 import { devPrint } from '../../components/utils/RandomUtils';
-import { Link } from 'react-router-dom';
-import { formatDate } from '../../localization';
 import { ReportView } from '../../components/admin/ReportView';
-
-const JsonDisplay = ({ data }: { data: string }) => (
-  <Box
-    bg="gray.900"
-    color="gray.50"
-    p={4}
-    borderRadius="md"
-    overflow="auto"
-    maxH="400px"
-    width="100%"
-  >
-    <pre>
-      <Code display="block" whiteSpace="pre" bg="transparent" color="gray.50">
-        {JSON.stringify(JSON.parse(data), null, 2)}
-      </Code>
-    </pre>
-  </Box>
-);
 
 const ReportDashboard = () => {
   const [reports, setReports] = useState<Report[]>([]);
@@ -44,8 +19,6 @@ const ReportDashboard = () => {
   const [searchKeyword, setSearchKeyword] = useState('');
   const [filterType, setFilterType] = useState<ReportType>();
   const [filterUserId, setFilterUserId] = useState<number>();
-  const [expandedReportId, setExpandedReportId] = useState<string>();
-  const [expandedJsonId, setExpandedJsonId] = useState<string>();
   const toast = useToast();
 
   useEffect(() => {
